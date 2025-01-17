7fb249927d59e3109cdc257dadfd7af6c2fac011
CCHobbit <Markus_Clephas@t-online.de>
2025-01-17 10:52:09 +0100

    Explizite Typenkonvertierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,15 +5,34 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-        output("Blick von aussen: " + cat);
-        output("Blick von innen: " + cat.getInstanceVariable());
+        // output("Blick von aussen: " + cat);
+        // output("Blick von innen: " + cat.getInstanceVariable());
+        
+        // Werte zuweisen
+        cat.firstName = "Paola";
+        // cat.furcolor = "Orange";
+        cat.age = 29;
 
+        // Werte ausgeben
+        output(cat.firstName);
+        output(cat.furcolor);
+        output(Integer.toString(cat.age));  // EXPLIZITE Typenkonvertierung
+        
         output("-----------------------");
 
         Cat cat2 = new Cat();
-        output("Blick von aussen: " + cat2);
-        output("Blick von innen: " + cat2.getInstanceVariable());
-
+        // output("Blick von aussen: " + cat2);
+        // output("Blick von innen: " + cat2.getInstanceVariable());
+        
+        // Werte zuweisung
+        cat2.firstName = "Pablo";
+        cat2.furcolor = "black";
+        // cat2.age = 26;
+        
+        // Werte ausgaben
+        output(cat2.firstName);
+        output(cat2.furcolor);
+        output(Integer.toString(cat2.age)); // EXPLIZITE Typenkonvertierung
     }
 
   
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,7 +2,17 @@ package com.cc.java;
 
 public class Cat {
 
+    String firstName;
+    String furcolor;
+    int age;
 
+
+   
+
+
+
+
+    
     public Cat getInstanceVariable(){
        return this;
     }
